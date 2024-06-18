4a5910cd6172d0070cc46dc461d9aeab217f8c1e
[nicowulf] <[n.wulfsztat@gmail.com]>
2024-06-18 19:44:34 -0300

    Add: search bar & fix fetch

--- Readme.md
+++ Readme.md
@@ -1,16 +1,17 @@
-## Search
-### <i class='bx bx-search-alt'></i>
 ## Location
-### <i class='bx bxs-location-plus'></i>
+<i class='bx bxs-location-plus'></i>
 
 ## Back
-### <i class='bx bx-left-arrow-alt'></i>
+<i class='bx bx-left-arrow-alt'></i>
+
 ## Next
-### <i class='bx bx-right-arrow-alt'></i>
+ <i class='bx bx-right-arrow-alt'></i>
+
 ## First
-### <i class='bx bx-arrow-to-left'></i>
+<i class='bx bx-arrow-to-left'></i>
+
 ## Last
-### <i class='bx bx-arrow-to-right' style='color:#ffffff' ></i>
+<i class='bx bx-arrow-to-right' style='color:#ffffff' ></i>
 
 ## PageUp
 ### <i class='bx bx-chevrons-up'></i>
--- app/personajes.js
+++ app/personajes.js
@@ -1,30 +1,56 @@
-const $main = document.querySelector("main");
+
+const $div = document.getElementById("grid");
+const $input = document.getElementById("filter");
+
 
 fetch("https://hp-api.onrender.com/api/characters")
-  .then((res) => {
-    return res.json();
-  })
+  .then((res) => res.json())
   .then((data) => {
-      data.forEach((character) => {
-                         
+    const characters = data;
+    
+    const renderCharacters = (characters) => {
+      $div.innerHTML = "";
+      characters.forEach((character) => {  
+        
         let status ;
           if (character.alive === true ? status = "Vivo" : status = "Muerto");
         
-        $main.innerHTML += `<div>
-        <img src="${
-          character.image
-            ? character.image
-            : "../assets/img/defaultChar/harry-potter-def-char.webp"
-        }">
-        <h4>${character.name}</h4>
-        <p><i>Casa</i>: ${character.house}</p>
-        <p><i>Especie</i>: ${character.species}</p>
-        <p><i>Género</i>: ${character.gender}</p>
-        <p><i>Estado</i>: ${status}</p>
-        <p><i>Intérprete</i>: ${character.actor}</p>
-        </div>`;
+        $div.innerHTML += `
+          <div class="card">
+            <div class="card_img">
+              <img src="${character.image ? character.image :
+               "../assets/img/defaultChar/harry-potter-def-char.webp"
+              }" alt="imagen de ${character.name}">
+            </div>
+            <div>
+              <h4>${character.name}</h4>
+              <p><i>Casa</i>: ${character.house}</p>
+              <p><i>Especie</i>: ${character.species}</p>
+              <p><i>Género</i>: ${character.gender}</p>
+              <p><i>Estado</i>: ${status}</p>
+              <p><i>Intérprete</i>: ${character.actor}</p>
+            </div>
+          </div>`
+      }
+    )};
+    renderCharacters(characters);
+    
+    $input.addEventListener("input", () => {
+    const searchValue = $input.value.toLowerCase();
 
+    const filterCharacter = characters.filter((character) =>
+      character.name.toLowerCase().includes(searchValue)
+    );
+
+    renderCharacters(filterCharacter); 
+  
     });
   });
+
+  
+ 
+
+
+
 
 
